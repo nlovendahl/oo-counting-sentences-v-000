be5439d36ef1208bd846b0f7023f22fbcd107efd
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-27 19:31:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_string = []
+    split_string << self.split(/[?.!]/)
+    no_empties = []
+    split_string.each do |x|
+      if x != "" && x != " "
+        no_empties << x
+      end
+    end
+    counted_string = no_empties.count
   end
 end
